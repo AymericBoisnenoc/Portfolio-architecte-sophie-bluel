cfaecfabf1c617760f25ab415e277726c9b058cf
Aymeric BOISNE-NOC <aymeric.boisnenoc@gmail.com>
2024-04-04 18:04:22 +0200

    mise a jour 1

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -30,39 +30,23 @@ document.addEventListener('DOMContentLoaded', function() {
                 const modale = document.createElement("div");
                 const modaleContent = document.createElement("div");
                 const modaleClose = document.createElement("span");
-                const closeBtn = document.createElement("button"); // Nouveau bouton pour fermer la modale
 
                 modale.classList.add('modal');
                 modale.id = "MaModale";
                 modaleContent.classList.add('modal-content');
 
-                function afficherGalerie(object, modaleContent) {
-                    let galleries = "";
-                    for (let work of object) {
-                        galleries += `
-                            <figure class="gallery-modal-work">
-                                <img src="${work.imageURL}">
-                                <i class="fa-solid fa-trash-can delete" id="${work.id}"></i>
-                                <figcaption>éditer</figcaption>
-                            </figure>`;
-                    }
-
-                    // Ajout de la galerie à la modaleContent
-                    modaleContent.innerHTML = galleries;
-                }
-
+                
                 // Appel de la fonction afficherGalerie avec votre tableau d'objets en tant que paramètre
                 afficherGalerie(data, modaleContent);
-
+                
                 // Ajouter ici le contenu de la modale en utilisant les données récupérées
-
+                
                 modaleClose.classList.add("close");
-
+                
                 modale.appendChild(modaleContent);
                 container.appendChild(modale);
-                modale.appendChild(modaleClose);
-                modale.appendChild(closeBtn); // Ajout du bouton pour fermer la modale
-
+                modaleContent.appendChild(modaleClose);
+                
                 modaleBtn.onclick = function() {
                     modale.style.display = "block";
                 }
@@ -73,13 +57,28 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                 }
 
-                closeBtn.textContent = "X"; // Texte du bouton pour fermer la modale
-                closeBtn.onclick = function() {
+                modaleClose.textContent = "X"; // Texte du bouton pour fermer la modale
+                modaleClose.onclick = function() {
                     modale.style.display = "none";
                 }
             })
             .catch(error => {
                 console.error('Erreur :', error);
             });
+        }
+    });
+    function afficherGalerie(object, modaleContent) {
+        let galleries = "";
+        for (let work of object) {
+            galleries += `
+                <figure class="gallery-modal-work">
+                    <img src="${work.imageUrl}">
+                    <i class="fa-solid fa-trash-can delete" id="${work.id}"></i>
+                    <figcaption>éditer</figcaption>
+                </figure>`;
+                console.log(object)
+        }
+
+        // Ajout de la galerie à la modaleContent
+        modaleContent.innerHTML = galleries;
     }
-});
